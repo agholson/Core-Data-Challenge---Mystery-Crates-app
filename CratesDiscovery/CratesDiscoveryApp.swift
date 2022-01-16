11ef173d13f509bd76f1332003ed2d9da4d8dcd6
Andrew Gholson <test@me.com>
2022-01-16 12:37:43 -0500

    Initial Commit

---
+++ CratesDiscovery/CratesDiscoveryApp.swift
@@ -0,0 +1,20 @@
+//
+//  CratesDiscoveryApp.swift
+//  CratesDiscovery
+//
+//  Created by Leone on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CratesDiscoveryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
